2394e4c3994a5e9d2fb850260a604580a12a5f99
Amogha Skanda Vishwanath <v.amoghaskanda@gmail.com>
2024-09-05 00:55:34 +0200

    First working commit

---
+++ Models/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace Pokedex.Models
+{
+    public class Config
+    {
+        public string? PokemonUrl { get; set; }
+        public string? YodaUrl { get; set; }
+        public string? ShakespeareUrl { get; set; }
+    }
+}
---
+++ Models/Species.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Pokedex.Models
+{
+    public class Species
+    {
+        public string? Name { get; set; }
+        public FlavorTextEntry?[] FlavorTextEntries
+        {
+            get { return FlavorTextEntries; }
+            set { Description = Regex.Replace(value[0].FlavorText ?? string.Empty, @"[\x00-\x1F]", " "); }
+        }
+
+        public HabitatEntry? Habitat { get; set; }
+        public string? Description { get; set; }
+        public bool IsLegendary { get; set; } = false;
+    }
+
+    public class FlavorTextEntry
+    {
+        public string? FlavorText { get; set; }
+    }
+
+    public class HabitatEntry
+    {
+        public string? Name { get; set; }
+    }
+}
